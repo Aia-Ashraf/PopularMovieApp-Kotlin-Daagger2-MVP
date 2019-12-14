8de2b4d40f1f8950a42aba35a70b178b54b59869
DELL <aia.ashraf94@gmail.com>
2019-12-14 19:39:24 +0200

    init

--- app/src/main/java/com/example/ahasan/kotlinmovieapp/MovieView.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ahasan.kotlinmovieapp
-
-import com.example.ahasan.kotlinmovieapp.Models.Result
-
-interface MovieView {
-
-    fun setResult(retrofitList: List<Result>) {
-    }
-}
